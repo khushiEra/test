2703a48721d977fc8aa17319e808fb4c68802cd7
Anand Suthar <anandsuthar@Anands-Mac-mini.local>
2018-08-16 11:41:58 +0530

    some changes master

--- demoProject/ViewController.swift
+++ demoProject/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // in master
         // some changes there
         // Do any additional setup after loading the view, typically from a nib.
     }
